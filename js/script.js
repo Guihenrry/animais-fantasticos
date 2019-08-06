1492725b8aef4384347f040ba315f316de8989ae
guihenrry <guilhermehenry60@gmail.com>
2019-08-06 09:23:22 -0300

    tootip refatorada

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/anima-tab.js';
 import Modal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
+import Tooltip from './modules/tooltip.js';
 import initDropDownMenu from './modules/dropdownmenu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFucionamento from './modules/funcionamento.js';
@@ -22,10 +22,9 @@ tabNav.init();
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
 
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
 
-modal.toggleModal();
-
-initTooltip();
 initDropDownMenu();
 initMenuMobile();
 initFucionamento();
